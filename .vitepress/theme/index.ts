e24d2a07d02a0834f8c3471a2c0aeec03242bd8f
coderjc <2740954762@qq.com>
2025-01-16 00:24:15 +0800

    feat: 添加图片放大功能

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,10 +1,34 @@
 import JcLayout from './Layout/index.vue'
 import Confetti from './components/confetti.ts'
+import MediumZoom from 'medium-zoom'
+import { onMounted, watch, nextTick } from 'vue'
+import { useRoute } from 'vitepress'
 import './styles/index.scss'
 
 export default {
     Layout: JcLayout,
     enhanceApp({ app, router }) {
         app.component('Confetti', Confetti)
+    },
+    setup() {
+        const route = useRoute()
+        const initZoom = () => {
+            nextTick(() => {
+                MediumZoom('.main img', {
+                    background: 'var(--mask-bg)'
+                })
+            })
+        }
+        onMounted(() => {
+            initZoom()
+        })
+        watch(
+            () => route.path,
+            () => {
+                nextTick(() => {
+                    initZoom()
+                })
+            }
+        )
     }
 }
