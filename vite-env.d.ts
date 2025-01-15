96df7e51abe9128cde67cd7bd24b87def6248b81
coderjc <2740954762@qq.com>
2025-01-15 14:59:07 +0800

    feat: 添加vite环境类型定义及更新.gitignore

---
+++ vite-env.d.ts
@@ -0,0 +1,11 @@
+interface ImportMetaEnv {
+    readonly BASE_URL: string
+    readonly DEV: boolean
+    readonly MODE: string
+    readonly PROD: boolean
+    readonly SSR: boolean
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
